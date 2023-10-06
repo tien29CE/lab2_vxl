3f11a059f39f1f44ee463ec8a30cfd4b89fa73ed
tien29CE <tien.le.290803@gmail.com>
2023-10-06 10:45:07 +0700

    ex5

---
+++ lab2_ex5/Core/Inc/software_timer.h
@@ -0,0 +1,18 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 27, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+extern int timer1_flag;
+
+void setTimer0(int duration);
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
